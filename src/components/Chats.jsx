c5b2da7a12debd7a35cf1952716af6baca68ffc9
HarshAnvekar <harshanvekar11@gmail.com>
2023-05-07 14:43:58 +0530

    Harsh changes

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,3 +1,4 @@
+//importing required modules
 import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
